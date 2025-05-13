ad80695a2861160fac321ab2cbbfc7c0e8d96045
Kyle Smith <me@yskkyle.com>
2025-05-12 21:40:46 -0400

    Fixed dark mode to work on every page

--- packages/ui/app/app.vue
+++ packages/ui/app/app.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
+import { useDark } from '@vueuse/core';
+
 const sidebarState = useCookie('sidebar:state');
+
+useDark();
 
 const defaultOpen = computed(() => (typeof sidebarState.value === 'boolean' ? sidebarState.value : sidebarState.value === 'true'));
 </script>
